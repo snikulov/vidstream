5d1d61195be1325640a0584cdca3e0285dd88560
VT-Step1 <VT-Step1@45b9b60e-f2be-47f7-962e-aa57ef663a13>
2014-03-23 11:55:19 +0000

    git-svn-id: https://xp-dev.com/svn/VT-1/branches/Corrupt_wrtiteframes@49 45b9b60e-f2be-47f7-962e-aa57ef663a13

---
+++ Corrupt.pro
@@ -0,0 +1,62 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-02-02T20:26:00
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = corrupt
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    bch.cpp \
+    corrupt.cpp \
+    ecc.cpp \
+    receiverthread.cpp \
+    restartblock.cpp \
+    senderthread.cpp \
+    split.cpp \
+    jpegops.cpp \
+    statcollector.cpp \
+    settingsdialog.cpp \
+    bitmap.cpp \
+    avhandler.cpp \
+    interlace.cpp \
+    settings.cpp
+
+HEADERS  += mainwindow.h \
+    bch.h \
+    corrupt.h \
+    ecc.h \
+    membuf.h \
+    receiverthread.h \
+    restartblock.h \
+    senderthread.h \
+    split.h \
+    jpegops.h \
+    statcollector.h \
+    settingsdialog.h \
+    bitmap.h \
+    avhandler.h \
+    interlace.h \
+    queue_params.h \
+    settings.h
+
+LIBS += -ljpeg -lavcodec -lavformat -lavdevice -lavutil -lswscale -lrt
+
+FORMS    += mainwindow.ui \
+    settingsdialog.ui
+
+#SUBDIRS += \
+#    test.pro
+
+OTHER_FILES += \
+    Corrupt.pro.user
+
+QMAKE_CFLAGS += -O3
+QMAKE_CXXFLAGS += -O3 -DGENERATE_HEADER -g -std=c++11 -Wno-write-strings
---
+++ ecc.cpp
@@ -0,0 +1,123 @@
+#include "ecc.h"
+
+#include "statcollector.h"
+
+#include <QDebug>
+
+using namespace std;
+
+ecc::ecc(uint8_t m, uint8_t t, StatCollector &stat) :
+    data_buff(NULL), errloc(NULL), stat(stat)
+{
+
+    ecc_init(m,t);
+}
+//============================================================================================================================
+void ecc::ecc_init(uint8_t m, uint8_t t){
+    pkg_len = (1 << m) / 8;
+    ecc_len = m*t/8;
+    if( (m*t) % 8 )
+        ecc_len += 1;
+    data_len = pkg_len - ecc_len;
+
+    if (data_len == 0 || pkg_len == 0 || ecc_len == 0) {
+        // will cause floating point exception in encode() and decode()
+        qCritical() << "Incorrect BCH parameters";
+        throw("Incorrect BCH parameters");
+    }
+
+    //cout<<" data:ecc ratio = 1:"<< (float)ecc_len / (float)data_len << " | ecc:data ratio = 1:"<< (float)data_len / (float)ecc_len<< endl;
+
+    pbch = init_bch(m, t, 0);
+
+    if(!pbch) {
+        qCritical() << "BCH init failed." << endl;
+        throw("BCH init failed");
+        //return;
+    }
+
+    if(data_buff) free(data_buff);
+    if(errloc) free(errloc);
+
+    data_buff = (uint8_t*)calloc(pkg_len, 1);
+    ecc_buff = data_buff + data_len;
+    errloc = (unsigned int*)calloc(pkg_len, sizeof(unsigned int));
+
+}
+
+//============================================================================================================================
+ecc::~ecc(){
+    if (pbch) {
+        free_bch(pbch);
+    }
+    if (data_buff) {
+        free(data_buff);
+    }
+    if (errloc) {
+        free(errloc);
+    }
+}
+
+//==================================================================
+void* ecc::encode(char* in_data, size_t in_data_len, size_t &out_data_len){
+
+    unsigned data_blocks = in_data_len / data_len;
+    uint8_t is_tail = 0;
+    if(in_data_len % data_len)
+        is_tail = 1;
+
+    char *data_pkg = (char *) calloc((data_blocks + is_tail) * pkg_len, 1);
+    for(unsigned curr_blk = 0; curr_blk < data_blocks; curr_blk++){
+        memcpy(data_buff, in_data + data_len*curr_blk, data_len);
+        memset(ecc_buff,0,ecc_len);
+        memset(errloc, 0, pkg_len*sizeof(unsigned int));
+        encode_bch(pbch, data_buff, data_len, ecc_buff);
+        memcpy(data_pkg + pkg_len * curr_blk, data_buff,  pkg_len);
+    }
+    if(is_tail){
+        memset(data_buff,0,pkg_len);
+        memcpy(data_buff, in_data + data_len * (in_data_len / data_len), in_data_len % data_len);
+        memset(errloc, 0, pkg_len*sizeof(unsigned int));
+
+        encode_bch(pbch, data_buff, data_len, ecc_buff);
+        memcpy(data_pkg + pkg_len * (in_data_len / data_len), data_buff,  pkg_len);
+    }
+
+    out_data_len = (data_blocks + is_tail) * pkg_len;
+    //fprintf(stderr, "out_data_len = %lu, in_data_len = %lu, "
+    //        "overhead = %0.2f%%, pkg_len = %u\n",
+    //        out_data_len, in_data_len,
+    //        (out_data_len - in_data_len) / (0.01 * in_data_len), pkg_len);
+    return data_pkg;
+}
+
+//==================================================================
+void* ecc::decode(char* in_data, size_t in_data_len, size_t &out_data_len, bool &successful){
+    unsigned data_blocks = in_data_len / pkg_len;
+    char *data_out = (char *) calloc(data_blocks, data_len);
+    successful = true;
+    unsigned failed_cnt = 0;
+    for(unsigned curr_blk = 0; curr_blk < data_blocks; curr_blk++){
+        memcpy(data_buff, in_data + pkg_len * curr_blk,  pkg_len);
+        int errors = decode_bch(pbch, data_buff, data_len, ecc_buff, 0, 0, errloc);
+
+        if (errors > 0) {
+             for (int ii = 0; ii < errors; ii++)
+                 if (errloc[ii] < 8*data_len)
+                     data_buff[errloc[ii]/8] ^= 1 << (errloc[ii] & 7);
+        }
+        if (errors < 0) {
+            successful = false;
+            failed_cnt++;
+        }
+
+
+        memcpy(data_out + data_len*curr_blk, data_buff, data_len);
+    }
+
+    stat.AddBchPkg(data_blocks, failed_cnt);
+
+    out_data_len = data_blocks * data_len;
+    return data_out;
+}
+
