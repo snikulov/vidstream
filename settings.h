51a1bb9e3fc84ea97af2133f5aed56e52ace8594
VT-Step1 <VT-Step1@45b9b60e-f2be-47f7-962e-aa57ef663a13>
2014-04-12 11:02:55 +0000

    git-svn-id: https://xp-dev.com/svn/VT-1/branches/Corrupt_t_p-92@94 45b9b60e-f2be-47f7-962e-aa57ef663a13

---
+++ settings.h
@@ -0,0 +1,40 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <string>
+#include <cstddef>
+
+struct Settings {
+    int lum_quality, chrom_quality;
+    int bch_m, bch_t;
+    size_t row_num, row_denom;
+    size_t block_num, block_denom;
+    size_t rst_block_size;
+
+    Settings() :
+        lum_quality(20),
+        chrom_quality(20),
+        bch_m(5), bch_t(4),
+        row_num(1), row_denom(1),
+        block_num(1), block_denom(1),
+        rst_block_size(1)
+    { }
+
+    Settings(int lum_quality, int chrom_quality,
+             int bch_m, int bch_t,
+             size_t row_num, size_t row_denom,
+             size_t block_num, size_t block_denom,
+             size_t rst_block_size) :
+        lum_quality(lum_quality),
+        chrom_quality(chrom_quality),
+        bch_m(bch_m), bch_t(bch_t),
+        row_num(row_num), row_denom(row_denom),
+        block_num(block_num), block_denom(block_denom),
+        rst_block_size(rst_block_size)
+    { }
+};
+
+void LoadSettingsFromFile(std::string filename, Settings &mode1, Settings &mode2);
+void SaveSettingsToFile(std::string filename, const Settings &mode1, const Settings &mode2);
+
+#endif // SETTINGS_H
---
+++ thread_decode.cpp
@@ -0,0 +1,51 @@
+#include "thread_decode.h"
+
+#include <errno.h>
+#include "ecc.h"
+#include "err.h"
+#include "pthread.h"
+#include "threaded_coder.h"
+#include "transport.h"
+#include <sys/types.h>
+#include <sys/errno.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
+
+#include <boost/interprocess/ipc/message_queue.hpp>
+
+using namespace boost::interprocess;
+
+DecoderThread::DecoderThread(ecc &coder,
+                             StatCollector &stat) :
+    coder(coder),
+    stat(stat),
+    killed(false)
+{ }
+
+void DecoderThread::run()
+{
+    message_queue input_que(open_or_create, TO_DECODE_MSG, NUM_OF_PKGS, PKG_MAX_SIZE);
+    message_queue output_que(open_or_create, TO_OUT_MSG, NUM_OF_PKGS, PKG_MAX_SIZE);
+
+    std::unique_ptr<uint8_t[]> recv_buf(new uint8_t[PKG_MAX_SIZE]);
+    DecodedBlock send_buf;
+    size_t recvd, out_lnt;
+    unsigned int priority;
+    std::vector<char> decoded;
+
+    while (!killed) {
+
+        input_que.receive(recv_buf.get(), PKG_MAX_SIZE, recvd, priority);
+
+        char* out_data = (char*)coder.decode((char *)(recv_buf.get()), recvd,
+                                             out_lnt, decoded, send_buf.decoded_ok);
+        stat.AddPacket(recvd, send_buf.decoded_ok);
+        send_buf.data_len = out_lnt;
+        memcpy(send_buf.data, out_data, out_lnt);
+        free(out_data);
+
+        output_que.send(&send_buf, out_lnt, 0);
+    }
+
+    return;
+}
