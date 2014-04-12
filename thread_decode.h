cbd28ef4748716df55add80f259ce20ce037355a
VT-Step1 <VT-Step1@45b9b60e-f2be-47f7-962e-aa57ef663a13>
2014-04-12 14:41:13 +0000

    git-svn-id: https://xp-dev.com/svn/VT-1/branches/Corrupt_t_p-92@97 45b9b60e-f2be-47f7-962e-aa57ef663a13

--- thread_decode.h
+++ thread_decode.h
@@ -6,19 +6,23 @@
 #include "ecc.h"
 #include "statcollector.h"
 #include "threaded_coder.h"
+#include <boost/interprocess/ipc/message_queue.hpp>
 
+namespace bipc = boost::interprocess;
+
+// corresponds to a single RestartBlock
 struct DecodedBlock
 {
     int data_len;
     bool decoded_ok;
-    uint8_t data[PKG_MAX_SIZE];
+    uint8_t data[PKG_MAX_SIZE]; // contains the RestartBlock
 };
 
 class DecoderThread : public QThread
 {
 public:
     DecoderThread(ecc &coder, StatCollector &stat);
-
+    void GetRSTFromPack(bipc::message_queue &output_que);
     void Kill() { killed = true; }
 protected:
     void run();
