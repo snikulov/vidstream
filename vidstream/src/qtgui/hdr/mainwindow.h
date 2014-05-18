4b8d15008d04515d3de9d23b32c17093b748c945
Sergei Nikulov <sergey.nikulov@gmail.com>
2014-05-18 12:16:47 +0400

    more ui stuff

--- vidstream/src/qtgui/hdr/mainwindow.h
+++ vidstream/src/qtgui/hdr/mainwindow.h
@@ -16,14 +16,36 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_clicked();
 
     void on_checkBox_is_gray_stateChanged(int arg1);
 
     void on_comboBox_camera_resolution_currentIndexChanged(int index);
 
+    void on_pushButton_operate_clicked();
+
+    void on_spinBox_rst_num_valueChanged(int arg1);
+
+    void on_spinBox_port_cmd_valueChanged(int arg1);
+
+    void on_spinBox_port_data_valueChanged(int arg1);
+
+    void on_lineEdit_channel_speed_textChanged(const QString &arg1);
+
+    void on_comboBox_bch_mode_currentIndexChanged(int index);
+
+    void on_spinBox_bch_m_valueChanged(int arg1);
+
+    void on_spinBox_bch_t_valueChanged(int arg1);
+
+    void on_spinBox_error_persent_valueChanged(int arg1);
+
+    void on_spinBox_lum_quality_valueChanged(int arg1);
+
+    void on_spinBox_chrome_quality_valueChanged(int arg1);
+
 private:
     Ui::MainWindow *ui;
+    bool is_srv_running_;
 };
 
 #endif // MAINWINDOW_H
--- vidstream/src/qtgui/src/mainwindow.cpp
+++ vidstream/src/qtgui/src/mainwindow.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <iostream>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    is_srv_running_(false)
 {
     ui->setupUi(this);
 }
@@ -13,17 +16,92 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
-{
-    // clicked Start button
-}
-
 void MainWindow::on_checkBox_is_gray_stateChanged(int arg1)
 {
     // grayscale toggled
+    std::cerr << "grayscale changed: " << arg1 << std::endl;
 }
 
 void MainWindow::on_comboBox_camera_resolution_currentIndexChanged(int index)
 {
     // current resolution changed
+    std::cerr << "camera resulutio index changed: " << index << std::endl;
+}
+
+void MainWindow::on_pushButton_operate_clicked()
+{
+    // run the program
+
+    // toggle flag
+    is_srv_running_ = is_srv_running_ ? !is_srv_running_ : !is_srv_running_;
+    if (is_srv_running_)
+    {
+        ui->pushButton_operate->setText("Стоп");
+    }
+    else
+    {
+        ui->pushButton_operate->setText("Запуск");
+    }
+    ui->spinBox_port_cmd->setDisabled(is_srv_running_);
+    ui->spinBox_port_data->setDisabled(is_srv_running_);
+}
+
+void MainWindow::on_spinBox_rst_num_valueChanged(int arg1)
+{
+    // rst value
+    std::cerr << "rst value changed: " << arg1 << std::endl;
+}
+
+void MainWindow::on_spinBox_port_cmd_valueChanged(int arg1)
+{
+    // control port
+    std::cerr << "control port value changed: " << arg1 << std::endl;
+}
+
+void MainWindow::on_spinBox_port_data_valueChanged(int arg1)
+{
+    // data port
+    std::cerr << "data port value changed: " << arg1 << std::endl;
+}
+
+void MainWindow::on_lineEdit_channel_speed_textChanged(const QString &arg1)
+{
+    // channel speed changed
+    std::cerr << "line speed changed" << std::endl;
+}
+
+void MainWindow::on_comboBox_bch_mode_currentIndexChanged(int index)
+{
+    // bch preset changed
+    std::cerr << "bch prameters changed: " << index << std::endl;
+}
+
+void MainWindow::on_spinBox_bch_m_valueChanged(int arg1)
+{
+    // bch m value
+    std::cerr << "bch m = " << arg1 << std::endl;
+}
+
+void MainWindow::on_spinBox_bch_t_valueChanged(int arg1)
+{
+    // bch t value
+    std::cerr << "bch t = " << arg1 << std::endl;
+}
+
+void MainWindow::on_spinBox_error_persent_valueChanged(int arg1)
+{
+    // line error changed
+    std::cerr << "line error " << arg1 << "%" << std::endl;
+}
+
+void MainWindow::on_spinBox_lum_quality_valueChanged(int arg1)
+{
+    // lum value changed
+    std::cerr << "lum value = " << arg1 << std::endl;
+}
+
+void MainWindow::on_spinBox_chrome_quality_valueChanged(int arg1)
+{
+    // chrome
+    std::cerr << "lum value = " << arg1 << std::endl;
 }
