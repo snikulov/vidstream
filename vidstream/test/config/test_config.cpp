b25a708517c042ba6597d214eb2bef1d4b761c38
Sergei Nikulov <sergey.nikulov@gmail.com>
2014-05-18 11:08:12 +0400

    checked config construction; now ready to transfer over the net

--- vidstream/test/config/test_config.cpp
+++ vidstream/test/config/test_config.cpp
@@ -34,6 +34,36 @@ BOOST_AUTO_TEST_CASE( test_config_case_1 )
     BOOST_CHECK(false == pt.get<bool>("cfg.img.bw"));
 }
 
+BOOST_AUTO_TEST_CASE( test_config_case_2 )
+{
+    boost::property_tree::ptree pt;
+
+    pt.put("cfg.dataport", 9955);
+    pt.put("cfg.bch.m", 5);
+    pt.put("cfg.bch.t", 103);
+    pt.put("cfg.img.width", 640);
+    pt.put("cfg.img.height", 480);
+    pt.put("cfg.img.rst", 1);
+    pt.put("cfg.img.lum", 20);
+    pt.put("cfg.img.chrom", 20);
+    pt.put("cfg.img.bw", false);
+
+    BOOST_CHECK(9955 == pt.get<int>("cfg.dataport"));
+    BOOST_CHECK(5 == pt.get<int>("cfg.bch.m"));
+    BOOST_CHECK(103 == pt.get<int>("cfg.bch.t"));
+
+    BOOST_CHECK(640 == pt.get<int>("cfg.img.width"));
+    BOOST_CHECK(480 == pt.get<int>("cfg.img.height"));
+    BOOST_CHECK(1 == pt.get<int>("cfg.img.rst"));
+    BOOST_CHECK(20 == pt.get<int>("cfg.img.lum"));
+    BOOST_CHECK(20 == pt.get<int>("cfg.img.chrom"));
+    BOOST_CHECK(false == pt.get<bool>("cfg.img.bw"));
+
+    std::ostringstream out;
+    write_json(out, pt, false);
+    BOOST_TEST_MESSAGE(out.str());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 
