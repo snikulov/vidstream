f2d2c3e87d45da2500fd81c096adecc1c71bb457
Sergey Nikulov <sergey.nikulov@gmail.com>
2014-10-15 18:06:42 +0400

    added itpp check - invest in api

---
+++ vidstream/test/itpp/test_itpp.cpp
@@ -0,0 +1,35 @@
+#define BOOST_TEST_MODULE test_perf
+#include <boost/test/included/unit_test.hpp>
+
+#include <itpp/itcomm.h>
+
+using namespace boost::unit_test;
+
+BOOST_AUTO_TEST_SUITE(test_suite_itpp)
+
+BOOST_AUTO_TEST_CASE( test_itpp_case_1 )
+{
+    int num_test = 0x0FF0;
+    itpp::bvec d2b = itpp::dec2bin(num_test);
+    int b2d  = itpp::bin2dec(d2b);
+    BOOST_CHECK_MESSAGE(num_test == b2d, "num_test = " << num_test << " b2d = " << b2d);
+
+    itpp::BCH bchcodec(7, 3);
+
+    itpp::bvec encoded = bchcodec.encode(d2b);
+
+    BOOST_MESSAGE("d2b.length = " << d2b.length());
+    BOOST_MESSAGE("encoded.length = " << encoded.length());
+
+    BOOST_CHECK_MESSAGE(d2b != encoded, "d2b = " << d2b << " encoded = " << encoded);
+
+    itpp::bvec decoded = bchcodec.decode(encoded);
+    BOOST_MESSAGE("decoded.length = " << decoded.length());
+
+    BOOST_CHECK_MESSAGE(d2b == decoded, "d2b = " << d2b << " decoded = " << decoded);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+
