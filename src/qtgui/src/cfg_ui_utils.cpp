09351f888611e2ea25fae8ac271fe817257b94bc
Sergei Nikulov <sergey.nikulov@gmail.com>
2015-02-25 17:31:48 +0300

    #23: fixed missed cfg.bw param

--- src/qtgui/src/cfg_ui_utils.cpp
+++ src/qtgui/src/cfg_ui_utils.cpp
@@ -262,6 +262,7 @@ void init_config_defaults(cfg_ptr_t cfg)
 
     cfg->put("cfg.cmdport", 9900);
     cfg->put("cfg.dataport", 9950);
+    cfg->put("cfg.bw", 10);
 
     cfg_set_resolution_by_list_index(*cfg, 2);
 }
